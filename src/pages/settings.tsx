d781b28c9dcd3f2b107a2de961dd792cff728f8e
Jayden <jayden666z@163.com>
2025-03-25 20:21:04 +0800

    fix: update URLs in configuration and components to use the correct domain and disable updater

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -38,31 +38,7 @@ const SettingPage = () => {
       title={t("Settings")}
       header={
         <ButtonGroup variant="contained" aria-label="Basic button group">
-          <IconButton
-            size="medium"
-            color="inherit"
-            title={t("Manual")}
-            onClick={toGithubDoc}
-          >
-            <HelpOutlineRounded fontSize="inherit" />
-          </IconButton>
-          <IconButton
-            size="medium"
-            color="inherit"
-            title={t("TG Channel")}
-            onClick={toTelegramChannel}
-          >
-            <Telegram fontSize="inherit" />
-          </IconButton>
-
-          <IconButton
-            size="medium"
-            color="inherit"
-            title={t("Github Repo")}
-            onClick={toGithubRepo}
-          >
-            <GitHub fontSize="inherit" />
-          </IconButton>
+          <></>
         </ButtonGroup>
       }
     >
